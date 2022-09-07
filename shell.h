24fcc8fe145f55229f19c22c25e9ed3d8f9d8e3d
George H. KYAMBADDE <94858400+ghmwesigwa@users.noreply.github.com>
2022-09-07 21:20:08 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -6,4 +6,15 @@
 #include <string.h>
 #include <unistd.h>
 
+/* function prototypes */
+/* ---------------String functions-------------------*/
+
+char *_strcat(char *dst, char *src);
+int _strcmp(char *, char *);
+int _strlen(char *);
+char *_strdup(char *s);
+char *_strndup(char *s, int n);
+
+/* ================================================== */
+
 #endif
