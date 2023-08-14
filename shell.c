07437a39cfb682191193ff28d70ace9e550a41c8
George KYAMBADDE <ghmwesigwa@gmail.com>
2023-08-14 07:37:04 -0700

    Adding Simple shell 0.2

--- shell.c
+++ shell.c
@@ -2,63 +2,14 @@
 #include <errno.h>
 
 /**
- * main - Entry point for the simple shell program.
- *
- * Description:
- *     This function implements a basic shell that displays a prompt,
- *     reads user input, and executes the entered commands using execve.
- *     It continues to display the prompt and wait for user input until
- *     the user enters Ctrl+D (EOF).
- *
- * Return: Always returns EXIT_SUCCESS.
- */
-int main(void)
-{
-	char *command;
-	size_t bufsize = MAX_COMMAND_LENGTH;
-
-	command = (char *)malloc(bufsize * sizeof(char));
-	if (command == NULL)
-	{
-		perror("Allocation error");
-		exit(EXIT_FAILURE);
-	}
-
-	while (1)
-	{
-		printf("#cisfun$ ");
-
-		/* Read user input */
-		getline(&command, &bufsize, stdin);
-
-		/* Remove newline character from the input */
-		command[strcspn(command, "\n")] = '\0';
-
-		/* Exit if Ctrl+D (EOF) is encountered */
-		if (feof(stdin))
-		{
-			printf("\n");
-			break;
-		}
-
-		execute_command(command);
-	}
-
-	free(command);
-	return (EXIT_SUCCESS);
-}
-
-/**
  * execute_command - Execute the given command using execve.
  * @command: The command to execute.
  *
  * Description:
- *     This function forks a child process and uses execve to
- *     execute the specified command. It waits for the child
- *     process to complete if it's the parent process.
+ * This function forks a child process and uses execve to
+ * execute the specified command. It waits for the child
+ * process to complete if it's the parent process.
  */
-#include "main.h"
-
 void execute_command(char *command)
 {
 	extern char **environ;
@@ -72,14 +23,21 @@ void execute_command(char *command)
 	}
 	if (child_pid == 0)
 	{
-		char *args[2];
+		char *args[MAX_ARGS];
+		char *token = strtok(command, " "); /* Split command into tokens */
 
-		args[0] = command;
-		args[1] = NULL;
+		int i = 0;
 
-		if (execve(command, args, environ) == -1)
+		while (token != NULL)
 		{
-			fprintf(stderr, "./shell: ");
+			args[i++] = token;
+			token = strtok(NULL, " ");
+		}
+		args[i] = NULL; /* Null-terminate the array */
+
+		if (execve(args[0], args, environ) == -1)
+		{
+			fprintf(stderr, "%s: ", args[0]);
 			perror(NULL);
 			exit(EXIT_FAILURE);
 		}
