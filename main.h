c4241ee5c803c73f66762c97c66e6d60ab4bc151
George KYAMBADDE <ghmwesigwa@gmail.com>
2023-08-14 07:20:50 -0700

    adding Simple shell 0.2

--- main.h
+++ main.h
@@ -3,17 +3,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
 
 #define MAX_COMMAND_LENGTH 1024
+#define MAX_ARGS 64
 
-/* main.c */
+/* Function prototypes */
 int main(void);
-
-/* shell.c */
 void execute_command(char *command);
 
 #endif /* MAIN_H */
