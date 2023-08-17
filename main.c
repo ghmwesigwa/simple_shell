6b7f8f9a3919aa623f078e6043bbdf18ced1dc25
George KYAMBADDE <ghmwesigwa@gmail.com>
2023-08-17 03:52:25 -0700

    Resolved merge conflicts

--- main.c
+++ main.c
@@ -47,6 +47,11 @@ int main(void)
  * @input: The user input.
  *
  * Description:
+<<<<<<< Updated upstream
+=======
+ * This function splits the input into arguments, handles built-in
+ * commands (exit and env), and executes other commands using execve.
+>>>>>>> Stashed changes
  */
 void handle_input(char *input)
 {
