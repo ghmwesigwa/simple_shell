b6e4ce5a53e4929710fef8ddaba9240c88951222
George KYAMBADDE <ghmwesigwa@gmail.com>
2023-08-17 10:29:32 -0700

    update fix-bulk funcs

--- builtins.c
+++ builtins.c
@@ -154,3 +154,31 @@ int find_alias_index(const char *name, char *alias_names[], int num_aliases)
     return -1;
 }
 
+/**
+ * replace_variables - Replace variable placeholders with their values.
+ * @args: The array of command-line arguments.
+ *
+ * Description:
+ * This function iterates through the array of arguments and replaces
+ * any variables of the form "$VAR" with their corresponding values
+ * from the environment. The replaced values are then updated in the
+ * same arguments array.
+ */
+void replace_variables(char *args[])
+{
+    int i;
+
+    for (i = 0; args[i] != NULL; i++)
+    {
+        if (args[i][0] == '$')
+        {
+            char *var_name = args[i] + 1; /* Skip the $ */
+            char *var_value = getenv(var_name);
+
+            if (var_value != NULL)
+            {
+                args[i] = var_value;
+            }
+        }
+    }
+}
